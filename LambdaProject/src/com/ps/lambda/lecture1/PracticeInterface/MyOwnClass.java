fe17bc449f686c394a64a5c2b191009e19e3c6a9
pradeep7421 <pradeep7421@gmail.com>
2025-07-25 21:05:10 +0530

    commit

--- LambdaProject/src/com/ps/lambda/lecture1/PracticeInterface/MyOwnClass.java
+++ LambdaProject/src/com/ps/lambda/lecture1/PracticeInterface/MyOwnClass.java
@@ -1,6 +1,6 @@
 package com.ps.lambda.lecture1.PracticeInterface;
 
-public class GittubPass {
+public class MyOwnClass{
 	public static void main(String[] args) {
    System.out.println("Hello");
 	}
