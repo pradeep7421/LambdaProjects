ab33c4a049ddafbb37ca685a6aed4d2fb6405b42
pradeep7421 <pradeep7421@gmail.com>
2025-07-25 21:19:18 +0530

    commit

--- LambdaProject/src/com/ps/lambda/lecture1/PracticeInterface/SumInterfTest.java
+++ LambdaProject/src/com/ps/lambda/lecture1/PracticeInterface/SumInterfTest.java
@@ -1,6 +1,6 @@
 package com.ps.lambda.lecture1.PracticeInterface;
 
-public class SumInterfImpl {
+public class SumInterfTest {
 
 	public static void main(String[] args) {
 		//Lambda expression with void returnType and passing argument as parameter
