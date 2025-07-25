0a7bad709d6eb324a79dbd26ce9b1804b0707a69
pradeep7421 <pradeep7421@gmail.com>
2025-07-25 20:09:55 +0530

    commit

--- LambdaProject/src/com/ps/lambda/lecture1/SumInterfImpl.java
+++ LambdaProject/src/com/ps/lambda/lecture1/SumInterfImpl.java
@@ -6,34 +6,38 @@ import com.ps.lambda.lecture1.PracticeInterface.SumInterf;
 public class SumInterfImpl {
 
 	public static void main(String[] args) {
-		//Lambda expression without returnType and passing argument as parameter
+		//Lambda expression with void returnType and passing argument as parameter
 		SquareInterFace squareInterFace = (int a)->{System.out.println((a*a));};
 		squareInterFace.printSquare(10);
-		//Lambda expression without returnType and passing argument as parameter without its type
+		//Lambda expression without void returnType and passing argument as parameter without its type
 		SquareInterFace squareInterFace2 = (a)->System.out.println((a*a));//for single statement curly bracket {} and type of parameter can be removed
 		squareInterFace2.printSquare(10);
 		
-		//Lambda expression without returnType and passing argument as parameter without its type and bracket
+		//Lambda expression with void returnType and passing argument as parameter without its type and bracket
 		SquareInterFace squareInterFace3 = a->System.out.println((a*a)); // for single argument ,argument bracket can be removed
 		squareInterFace3.printSquare(10);
 		
-		//By using lambda exp for multiple line of code with return type
+		//By using lambda exp for multiple line of code with int return type
 		SumInterf sumInterf = (int a,int b)->{
 		int sum = a+b;
 		System.out.println("sum of two numbers -"+sum);
 		return sum;
 		};
-				 
-		//By using lambda exp with  return token without parameter type
+		System.out.println(sumInterf.sum(10, 20));	
+		
+		//By using lambda exp with int return token without parameter type
 		SumInterf sumInterf2 = (a,b)-> {
 			return (a+b);
 			};
+			System.out.println(sumInterf2.sum(30, 20));		
 			
-		//By using lambda exp with variable type in parameter without return token 
+		//By using lambda exp with variable type in parameter without return keyword in implementation 
 		SumInterf sumInterf3 = (int a,int b)->(a+b); //remove return keyword for single line of code
-	
+		System.out.println(sumInterf3.sum(0, 2));	
+		
 		//By using lambda exp without variable type in parameter without return keyword 
 		SumInterf sumInterf4 = (a,b)->(a+b);
+		System.out.println(sumInterf4.sum(1, 0));	
 				
 		
 	}
