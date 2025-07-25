a81d294f6d0fa09c33d49b08ed0ed3658278dca8
pradeep7421 <pradeep7421@gmail.com>
2025-07-25 20:59:58 +0530

    commit

--- LambdaProject/src/com/ps/lambda/lecture1/PracticeInterface/GitubPasKey.java
+++ LambdaProject/src/com/ps/lambda/lecture1/PracticeInterface/GitubPasKey.java
@@ -2,7 +2,7 @@ package com.ps.lambda.lecture1.PracticeInterface;
 
 public class GitubPasKey {
 	public static void main(String[] args) {
-
+System.out.println("----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------");
 	}
 }
 
